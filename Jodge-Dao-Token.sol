05d07779b0aef278862ba07169669e43eae42df4
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2022-10-29 13:39:08 -0700

    Create Jodge-Dao-Token.sol

---
+++ Jodge-Dao-Token.sol
@@ -0,0 +1,39 @@
+//This is the governance token for the Jodge-DAO. The max supply is 16million tokens.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Jodge_DAO is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
+    constructor() ERC20("Jodge_DAO", "JD") ERC20Permit("Jodge_DAO") {
+        _mint(msg.sender, 16000000 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
